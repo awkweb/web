848509e87bf3545208b1cba7ab17ebec06d4275d
Tom Meagher <thom.meagher@gmail.com>
2019-03-19 08:56:20 -0400

    Fix prod api base url

--- src/api/init.ts
+++ src/api/init.ts
@@ -2,17 +2,17 @@ import { camelize } from "@ridi/object-case-converter";
 import axios from "axios";
 
 const baseURL =
-    process.env.NODE_ENV === "production"
-        ? "https://api.wilbur.local/v1/"
-        : "https://api.wilbur.local/v1/";
+  process.env.NODE_ENV === "production"
+    ? "https://api.wilbur.app/v1/"
+    : "https://api.wilbur.local/v1/";
 
 export default axios.create({
-    baseURL,
-    transformResponse: [
-        data => {
-            if (data) {
-                return camelize(JSON.parse(data), { recursive: true });
-            }
-        }
-    ]
+  baseURL,
+  transformResponse: [
+    data => {
+      if (data) {
+        return camelize(JSON.parse(data), { recursive: true });
+      }
+    }
+  ]
 });
